73965a4911f4c9fba404c208dc4212a4950f8235
Anik Kumar Samanta <ece.anik@gmail.com>
2020-11-01 09:57:14 +0530

    011120

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,20 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+// Function to expand text (AKS)
+function expandText() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
